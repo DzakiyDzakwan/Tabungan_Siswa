557afef365fbf12dc43fbd3f39bcff7945e68803
Febri Zuladhari <febrizuladhari12345@gmail.com>
2022-05-29 22:00:47 +0700

    sorry salah" -febri

--- tabungan-siswa/app/Http/Controllers/HomeController.php
+++ tabungan-siswa/app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
 {
     public function show() {
 
-        $posts = DB::table();
+        $posts = DB::table('beritas');
         if(request('cari')) {
             $posts->where('judul', 'like', '%' . request('cari') . '%')
                 ->orWhere('author', 'like', '%' . request('cari') . '%');
@@ -21,12 +21,12 @@ class HomeController extends Controller
             
     }
 
-    public function cari(Request $request) {
+    // public function cari(Request $request) {
 
-        // $cari = $request->cari;
-        // $posts = DB::table('beritas')->where('judul', 'like', "%".$cari."%")->paginate();
-        //     return view('user.home', ['beritas' => $posts]);
-    }
+    //     $cari = $request->cari;
+    //     $posts = DB::table('beritas')->where('judul', 'like', "%".$cari."%")->paginate();
+    //         return view('user.home', ['beritas' => $posts]);
+    // }
 
 
 }
